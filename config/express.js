d315ba02881e7f037e33f094adbf715e63b07158
ysmaili.freitas <ysmaili.freitas@azi.com.br>
2022-07-11 17:11:11 -0400

    first commit

---
+++ config/express.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var load = require('express-load')
+
+module.exports = function() {
+  var app = express();
+
+  app.set('port', 3000);
+
+  app.use(express.static('./public'));
+  app.set('view engine', 'ejs');
+  app.set('views','./app/views/');
+
+  load('models',{cwd: 'app'})
+      .then('controllers')
+      .then('routes')
+      .into(app)
+
+  return app;
+};
